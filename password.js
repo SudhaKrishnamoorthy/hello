706157045bd4ad6c7abef66c1a953ea37284b74a
Sudha Krishnamoorthy <sudhavkr@hotmail.com>
2022-08-27 14:33:09 -0500

    Initial commit password

---
+++ password.js
@@ -0,0 +1,16 @@
+var password = "sudhalovescoding2022"
+var letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+let i = 0
+
+if (password.length < 10) {
+    console.log("Your password change is succesful")
+}
+
+
+else if (/\d/.test("sudhalovescoding2022")) {
+    console.log("Your password change was successful")
+}
+
+else if (password.includes(letter[i])) {
+    console.log("Your password chage was successful")
+}
