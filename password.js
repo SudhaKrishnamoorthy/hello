9f8a508d517af20acc664e981ea8703cc3078b09
Sudha Krishnamoorthy <sudhavkr@hotmail.com>
2022-08-27 15:22:55 -0500

    password validaror commit additional reqs

--- password.js
+++ password.js
@@ -1,19 +1,12 @@
-var password = "sudhalovestocode2022"
-//var letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-//let i = 0
-
+var password = "SUDHALOVESTOCODE20 22"
+//Basic tests
 //if (password.length > 10 && /\d/.test(password) && password.includes(letter[i]) ) {
    // console.log("Your password change was successful")
 //}
-if (password.length > 10 && /\d/.test(password) && /^[A-Za-z]/)  {
+// Additional checks for if the password is all uppercase and if the password contains a space
+if (password.length > 10 && /\d/.test(password) && /[A-Za-z]/ && (password.toUpperCase() === password) && /\s/.test(password))  {
     console.log("Your password change was successful")
  }
 
-/*else if (/\d/.test("sudhalovescoding2022")) {
-    console.log("Your password change was successful")
-}
 
-else if (password.includes(letter[i])) {
-    console.log("Your password chage was successful")
-}*/
 else {console.log("Your password change was not successful, try again")}
